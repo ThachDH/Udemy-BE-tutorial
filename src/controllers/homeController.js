37c812febb6d8a55135477efcfd8a1d654496c86
Thach <hoducthach123@gmail.com>
2023-05-02 12:13:57 +0700

    video 35

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,20 +2,7 @@
 const connection = require('../config/database');
 
 const getHomepage = (req, res) => {
-  console.log('he')
-  let users = [];
-  // process data
-  // call model
-  // simple query
-  connection.query(
-    'select*from Users u',
-    function (err, results, fields) {
-      users = results;
-      console.log('results', results); // results contains rows returned by server
-      res.send(JSON.stringify(users));
-
-    }
-  );
+  return res.render('home.ejs')
 }
 
 const getTest = (req, res) => {
